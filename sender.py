d838724b69dae32427d17ccb487c0d24d5ba5664
macnoblebk <macnoblebk@gmail.com>
2024-05-13 21:44:07 -0700

    Adds code documentation

--- sender.py
+++ sender.py
@@ -8,39 +8,27 @@ SLEEP_DURATION = 3
 
 class Sender:
     def __init__(self):
-        """ 
-        Your constructor should not expect any argument passed in,
-        as an object will be initialized as follows:
-        sender = Sender()
-        
-        Please check the main.py for a reference of how your function will be called.
-        """
+        """Initializes the Sender class with a UDP socket and binds it to the sender address."""
         self.seq_num = 0
         self.packet_counter = 0
 
     def rdt_send(self, app_msg_str):
-        """realibly send a message to the receiver (MUST-HAVE DO-NOT-CHANGE)
-
-        Args:
-          app_msg_str: the message string (to be put in the data field of the packet)
-
+        """
+            Creates a package given input message and sends packet to UDP socket
+            Args:
+                app_msg_str (str): The message to be sent.
         """
         print(f"\noriginal message string: {app_msg_str}")
         packet = make_packet(app_msg_str, 0, self.seq_num)
         print(f"packet created: {packet}")
         self.send_packet(packet, app_msg_str)
 
-    ####### Your Sender class in sender.py MUST have the rdt_send(app_msg_str)  #######
-    ####### function, which will be called by an application to                 #######
-    ####### send a message. DO NOT change the function name.                    #######
-    ####### You can have other functions if needed.                             #######
-
     def send_packet(self, packet, app_msg_str):
         """
-        Sends a packet to the receiver, waits for an acknowledgment, and handles retransmissions if necessary.
-        Args:
-            packet (bytes): The packet data to be sent.
-            app_msg_str (str): The application message string being sent, used for logging.
+            Sends a packet to the receiver, waits for an acknowledgment, and handles retransmissions if necessary.
+            Args:
+                packet (bytes): The packet data to be sent.
+                app_msg_str (str): The application message string being sent, used for logging.
         """
         sender_socket = socket(AF_INET, SOCK_DGRAM)
         sender_socket.settimeout(SLEEP_DURATION)
