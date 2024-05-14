78e70fc1cf73ccc27e43cb3fef1cd0374c529ebe
macnoblebk <macnoblebk@gmail.com>
2024-05-13 21:34:07 -0700

    Implements receiver.py class

--- receiver.py
+++ receiver.py
@@ -1,3 +1,14 @@
 from socket import *
 from time import sleep
-## No other imports allowed
+
+BUFFER_SIZE = 1024
+ADDRESS = ('', 10185)
+SLEEP_DURATION = 5
+TIMEOUT_FREQUENCY = 6
+PACKET_CORRUPTION_FREQUENCY = 3
+class Receiver:
+    def __init__(self):
+        self.receiver_socket = socket(AF_INET, SOCK_DGRAM)
+        self.receiver_socket.bind(ADDRESS)
+        self.previous_seq_num = -1
+        self.packet_counter = 0
